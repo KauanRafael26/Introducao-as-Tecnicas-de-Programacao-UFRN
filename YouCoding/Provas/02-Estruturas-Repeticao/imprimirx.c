7149fb1008503070cfbfc3f4fcf0df4fa03adda2
KauanRafael26 <kauanrafaelcd1@gmail.com>
2026-08-31 11:57:51 -0300

    ajustes 3

---
+++ YouCoding/Provas/02-Estruturas-Repeticao/imprimirx.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+  int n; scanf("%d", &n);
+  char c; scanf("%c", &c);
+
+
+  for (int i = 0; i < n - 1; i++) {
+    break;
+  }
+
+  printf("Teste");
+  printf("Teste 2");
+  printf("Teste 3\n");
+  printf("Teste 4");
+  printf("Teste5\n");
+
+  return 0;
+}
